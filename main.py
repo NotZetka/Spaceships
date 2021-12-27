62263e6160c8c8b7be3e2f7e0d53f97f2c6e970b
Zetka <jakub.kochanski10@gmail.com>
2021-12-27 09:13:46 +0100

    finished

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import random
 
 WIDTH = 400
 HEIGHT = 600
-
 FPS = 120
+BULLETS = 5
+
 
 BLACK=(0,0,0)
+WHITE=(255,255,255)
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Spaceships by Jakub Kochański')
@@ -15,8 +17,15 @@ SPACESHIP = pygame.transform.scale(pygame.image.load('assets/spaceship.png'),(30
 ALIEN = pygame.transform.scale(pygame.image.load('assets/alien.png'),(30,30))
 BACKGROUND = pygame.transform.scale(pygame.image.load('assets/background.png'),(400,600))
 
-spaceship = pygame.Rect(185,550,30,30)
-aliens = []
+def lose_game():
+    pygame.font.init()
+    text = pygame.font.SysFont('comicsans', 20)
+    text = text.render(f'wynik {score}',1,WHITE)
+    WIN.blit(text,(WIDTH/2 - text.get_width() /2, HEIGHT/2 - text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(3000)
+    pygame.quit()
+    return False
 
 def spaceship_movement(keys_pressed):
     if keys_pressed[pygame.K_LEFT] and spaceship.x>0:
@@ -27,40 +36,70 @@ def spaceship_movement(keys_pressed):
         spaceship.y-=3
     if keys_pressed[pygame.K_DOWN] and spaceship.y+spaceship.height<HEIGHT:
         spaceship.y+=3
+
+def time_handling():
+    global time
+    global score
+    global counter
+    pygame.time.Clock().tick(FPS)
+    if time % 2 == 0:
+        for alien in aliens:
+            alien.y += 1
+        for bullet in bullets:
+            for alien in aliens:
+                if bullet.colliderect(alien):
+                    print('trafiony')
+                    bullets.remove(bullet)
+                    aliens.remove(alien)
+                    score+=1
+                    if counter>30:
+                        counter-=2
+            if bullet.y<=0:
+                bullets.remove(bullet)
+
+
+            bullet.y-=10
+    if time % counter == 0:
+        aliens.append(pygame.Rect(random.randrange(0, 370), 0, 30, 30))
+    time += 1
+def draw_window():
+    WIN.blit(BACKGROUND, (0, 0))
+    for alien in aliens:
+        WIN.blit(ALIEN, (alien.x, alien.y))
+    for bullet in bullets:
+        pygame.draw.rect(WIN,WHITE,bullet)
+    keys_pressed = pygame.key.get_pressed()
+    spaceship_movement(keys_pressed)
+    WIN.blit(SPACESHIP, (spaceship.x, spaceship.y))
+    pygame.display.update()
+
+spaceship = pygame.Rect(185,550,30,30)
+aliens = []
+bullets = []
 score = 0
 time  = 0
 counter = 80
 
 def main():
-    global time
     run = True
     while run:
-        if time%2==0:
-            for alien in aliens:
-                alien.y+=1
-        if time%counter == 0:
-            aliens.append(pygame.Rect(random.randrange(0,370),0,30,30))
-        time+=1
-        pygame.time.Clock().tick(FPS)
-        WIN.blit(BACKGROUND,(0,0))
-        for alien in aliens:
-            WIN.blit(ALIEN,(alien.x,alien.y))
-        keys_pressed = pygame.key.get_pressed()
-        spaceship_movement(keys_pressed)
-        WIN.blit(SPACESHIP,(spaceship.x,spaceship.y))
-        pygame.display.update()
-
-        for alien in aliens:
-            if alien.y>=HEIGHT:
-                pygame.quit()
-                run = False
-            elif spaceship.colliderect(alien):
-                pygame.quit()
-                run = False
+        time_handling()
+        draw_window()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = lose_game()
+                break
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and len(bullets)<=BULLETS:
+                    bullet = pygame.Rect(spaceship.x+spaceship.width//2,spaceship.y,2,10)
+                    bullets.append(bullet)
         if run:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    run = False
+            for alien in aliens:
+                if alien.y+spaceship.height>=HEIGHT:
+                    run = lose_game()
+                elif spaceship.colliderect(alien):
+                    run = lose_game()
+
+
 
 main()
